af7a856e7bea6e528394828eced0ee10a84db9e7
jedrekszor <jedrekszor@wp.pl>
2026-04-08 12:38:31 +0200

    responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorPalette="blue">test</Button>;
+  const isLarge = useBreakpointValue({ base: false, lg: true });
+
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "side main"`,
+      }}
+    >
+      <GridItem area={"nav"} bg={"red"}>
+        Navbar
+      </GridItem>
+      <Show when={isLarge}>
+        <GridItem area={"side"} bg={"blue"}>
+          Side
+        </GridItem>
+      </Show>
+      <GridItem area={"main"} bg={"green"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
