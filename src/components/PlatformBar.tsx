aab89b392dc435cad1f10ad54b9edba054f35919
jedrekszor <jedrekszor@wp.pl>
2026-04-13 20:53:07 +0200

    platform icons

---
+++ src/components/PlatformBar.tsx
@@ -0,0 +1,40 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsNintendoSwitch, BsGlobe } from "react-icons/bs";
+import type { Platform } from "../hooks/useGames";
+import type { IconType } from "react-icons";
+
+interface Props {
+  platforms: Platform[];
+}
+const PlatformBar = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    nintendo: BsNintendoSwitch,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1.5}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformBar;
