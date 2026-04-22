d6f74d1f985faa528713b763d8dc0ceacf7a00b1
jedrekszor <jedrekszor@wp.pl>
2026-04-22 15:30:00 +0200

    refactor

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card.Root borderRadius={10} overflow={"hidden"} width={"300px"}>
+    <Card.Root>
       <Skeleton height={"200px"} />
       <CardBody>
         <SkeletonText />
